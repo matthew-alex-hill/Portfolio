db97a5c220836847c572b880227e33b793f7cf4d
Matthew Hill <67704019+matthew-alex-hill@users.noreply.github.com>
2021-08-02 22:18:52 +0100

    Added Source code files

---
+++ SAMPLE(var).php
@@ -0,0 +1,4 @@
+<?php
+$OESiteUploadDirectory = "Upload";
+$OEConfWETextBox = '{"WE5463b4f31c":{"InputName":"WE5463b4f31c"},"WEb2e359c61b":{"InputName":"WEb2e359c61b"},"WE2fecd85dbb":{"InputName":"WE2fecd85dbb"},"WE8a01985835":{"InputName":"WE8a01985835"}}';
+$OEConfWETextArea = '{"WEe26d56f40b":{"InputName":"WEe26d56f40b"}}';
